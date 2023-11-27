ac6876b67210609e22b83b695fae47a9c2159ac0
zeted0410 <148453706+zeted0410@users.noreply.github.com>
2023-11-27 21:40:55 +1030

    Create main.js

---
+++ main.js
@@ -0,0 +1,9 @@
+function changeBtnName()  {
+  const btnElement 
+    = document.getElementById('btn');
+  
+  const html 
+    = '<div style="color:red"> 새이름!! </div>';
+  
+  btnElement.innerHTML = html;
+}

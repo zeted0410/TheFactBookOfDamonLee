d553d6ec89295b5c2c5e13d89b40963cd9448eaa
zeted0410 <148453706+zeted0410@users.noreply.github.com>
2023-12-02 09:27:34 +1030

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 function changeBtnName()  {
   const btnElement 
-    = document.getElementById('btn');
+    = document.getElementById('sent1');
   
   const html 
-    = '<div style="color:red"> 새이름!! </div>';
+    = '<div> Damon is handsome. </div>';
   
   btnElement.innerHTML = html;
 }

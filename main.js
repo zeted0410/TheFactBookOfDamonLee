5ea966fe03119ffefd94d1d5e6004c1f38c6303a
zeted0410 <148453706+zeted0410@users.noreply.github.com>
2023-12-02 09:56:08 +1030

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 function changeBtnName()  {
-  var i=0
-  if(i==0) {
+  var i="0"
+  if(i=="0") {
     document.getElementById("sent1").innerHTML = "Damon is handsome"
-    i=1
+    i="1"
   } else {
     document.getElementById("sent1").innerHTML = "Damon is the ugliest person in the world."
-    i=0
+    i="0"
   }
 }

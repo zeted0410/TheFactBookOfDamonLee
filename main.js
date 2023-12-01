044b5227a2e3d4f4149e241658e62a503af518d7
zeted0410 <148453706+zeted0410@users.noreply.github.com>
2023-12-02 09:32:57 +1030

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,3 @@
 function changeBtnName()  {
-  const btnElement 
-    = document.getElementById('sent1');
-  
-  const html 
-    = '<h1> Damon is handsome. </h1>';
-  
-  btnElement.innerHTML = html;
+  document.getElementById("sent1").innerHTML = "Damon is handsome"
 }

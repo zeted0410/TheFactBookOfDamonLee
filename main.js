9e39e205bb4b85aa2b2f1c8d2e413a24da12f726
zeted0410 <148453706+zeted0410@users.noreply.github.com>
2023-12-02 22:18:26 +1030

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,21 @@
 const texts = document.querySelectorAll('#texts p');
 
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if(entry.isIntersecting) {
+      entry.target.style.transform = 'translateY(0)';  
+      entry.target.style.opacity = 1;
+    }
+  });
+}, {
+  threshold: 0.5
+});
+
 texts.forEach(text => {
-  text.style.opacity = 0;
-  
-  setTimeout(() => {
-    text.style.opacity = 1;
-  }, 100 * text.innerHTML.length);
+  observer.observe(text);
+});
+
+// Optional scroll to top
+window.addEventListener('DOMContentLoaded', () => {
+  window.scrollTo(0, 0);
 });
